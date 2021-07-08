6f37f1ae140819d47980a9d9c52a55d9085afb90
i-vikk <48582273+i-vikk@users.noreply.github.com>
2021-07-08 10:24:56 +0530

    Add files via upload

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div className="nav">
+        <img 
+            className="nav__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+            alt="Netflix_logo" 
+            />
+            
+        </div>
+    )
+}
+
+export default Nav;
